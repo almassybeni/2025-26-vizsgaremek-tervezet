0c1789a7c28dc7951554aecb32f949e414ad7dfd
almassybeni <almassy.benedek.solt@gmail.com>
2026-04-09 16:46:17 +0200

    docker

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,25 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
+  // A process.env kiolvassa a docker-compose.yml-ből az adatokat!
+  // Ha nem Dockerből indítod, akkor az "or" (||) utáni alapértékeket használja.
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '', 
   database: process.env.DB_NAME || 'kulturvadasz',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-const promisePool = pool.promise();
+// Teszteljük a kapcsolatot induláskor
+db.getConnection()
+  .then(connection => {
+    console.log('✅ Sikeresen csatlakozva a MySQL adatbázishoz!');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('❌ Hiba az adatbázis csatlakozáskor:', err.message);
+  });
 
-module.exports = promisePool;
+module.exports = db;
